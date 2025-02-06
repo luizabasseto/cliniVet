c5b53db88ef9ee3b9aaeb62d0d1db404cdc7c36c
carolinelauu <carolinelau005@gmail.com>
2025-02-06 20:54:23 -0300

    nome do DAO estava errado

--- daos/consultaDao.hpp
+++ daos/consultaDao.hpp
@@ -1,8 +1,8 @@
 #include <vector>
 #include "consulta.hpp"  
 
-class DaoConsulta{
-    std::vector<Consulta> consultas= {
+class consultaDao{
+    std::vector<Consulta> consultas = {
         Consulta(1, {10, 12, 2024}, "Consulta de Rotina", 22.5, 'A', new Animal(1, "Rex", {15, 8, 2015}, 'M')),
         Consulta(2, {12, 12, 2024}, "Exame de Sangue", 25.0, 'P', new Animal(2, "Bella", {20, 5, 2017}, 'F'))
     };;
--- daos/managerDao.hpp
+++ daos/managerDao.hpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-class DaoManager{
+class managerDao{
     private:
     
     public:
